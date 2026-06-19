233b09ee9c5a79fb6a5ca1a73a3557587f32caab
Alex Cutler <calexcutler@gmail.com>
2026-06-19 15:45:32 -0600

    Updated library and app versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.cacutler.cardgamepointtracker"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildTypes {
